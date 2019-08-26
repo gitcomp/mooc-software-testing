f84dfb4e2838b507f83b9d6fed6c29b78e01ae39
Ahmet Duman <Ahmet.Duman@s.pioneercss.org>
2019-08-26 14:17:02 -0400

    aded addFive class

--- src/main/java/tudelft/gettingstarted/GettingStarted.java
+++ src/main/java/tudelft/gettingstarted/GettingStarted.java
@@ -4,4 +4,12 @@ public class GettingStarted {
     public int addFive (int number) {
         return number + 5;
     }
+
+    
+//public static void main(String[] args) {
+//	GettingStarted gs = new GettingStarted();
+//	int sum= gs.addFive(2);
+//	System.out.println(sum);
+//}
+
 }
--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,5 +1,7 @@
 package tudelft.gettingstarted;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -11,17 +13,46 @@ public class GettingStartedTest {
         Assertions.assertEquals(25,result);
     }
 
+    //above same as this:
+    @Test
+    public void add20toIt() {
+    	GettingStarted gs = new GettingStarted();
+    	int actualResult = gs.addFive(20);
+    	int expectedResult = 25;
+    	Assertions.assertEquals(expectedResult, actualResult);
+    }
+    
+    
 //    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToZero() {
+        int result = new GettingStarted().addFive(0);
+        Assertions.assertEquals(5, result);
+    }
+
+    //above same as this
+    @Test
+    public void addzerotoit() {
+    	GettingStarted gs = new GettingStarted();
+    	int actualResult=gs.addFive(0);
+    	int expectedResult= 5;
+    	Assertions.assertEquals(expectedResult, actualResult);
+    }
+    
+        
+    @Test
+    public void addFiveToMinus20() {
+        int result = new GettingStarted().addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
+    //above same as this
+    @Test
+    public void addminus20toit(){
+    	int actualResult= new GettingStarted().addFive(-20);
+    	int expectedResult= -15;
+    	Assertions.assertEquals(expectedResult, actualResult);
+    	
+    }
+        
 }
